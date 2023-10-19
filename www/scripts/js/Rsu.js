39a1b09350edbdd67666577a13ad57af0a5b59b3
choutassou <choutassou@gmail.com>
2023-10-19 11:20:26 +0900

    backup

--- www/scripts/js/Rsu.js
+++ www/scripts/js/Rsu.js
@@ -1,41 +1,29 @@
-Ki67 = 0.0
-STK15 = 0.0
-Survivin = 0.0
-CyclinB1 = 0.0
-MYBL2 = 0.0
-GRB7 = 0.0
-HER2 = 0.0
-GSTM1 = 0.0
-CD68 = 0.0
-BAG1 = 0.0
-ER = 0.0
-PGR = 0.0
-Bcl2 = 0.0
-SCUBE2 = 0.0
-Stromelysin3 = 0.0
-CathepsinL2 = 0.0
-BetaActin = 0.0
-GAPDH = 0.0
-RPLP0 = 0.0
-GUS = 0.0
-TFRC = 0.0
+function calc_rsu(data)
+{
+    //group HER2
+    data["g-HER2"] = data["GRB7"] * 0.9 + data["HER2"] * 0.1
+    if (data["g-HER2"] < 8.0) {
+        data["g-HER2"] = 8.0
+    }
+    
+    //group ESTROGEN
+    data["g-ESTROGEN"] = (data["ER"] * 0.8 + data["PGR"] * 1.2 + data["Bcl2"] + data["SCUBE2"]) / 4.0
 
-HER2GroupScore = GRB7 * 0.9 + HER2 * 0.1
-if (HER2GroupScore < 8.0) {
-    HER2GroupScore = 8.0
+    data["g-PROLIFERATION"] = (
+        data["Survivin"] + data["Ki-67"] + data["MYBL2"] + data["Cyclin-B1"] + data["STK15"]) / 5.0
+    if (data["g-PROLIFERATION"] < 6.5) {
+        data["g-PROLIFERATION"] = 6.5
+    }
+    data["g-INVASION"] = (data["Cathepsin-L2"] + data["Stromelysin-3"]) / 2.0
+    
+    data["RSU"] = Math.round((0.47 * data["g-HER2"]
+        - 0.34 * data["g-ESTROGEN"]
+        + 1.04 * data["g-PROLIFERATION"]
+        + 0.10 * data["g-INVASION"]
+        + 0.05 * data["CD68"]
+        - 0.08 * data["GSTM1"]
+        - 0.07 * data["BAG1"])*100)/100
+    
+    return data
 }
 
-ERGroupScore = (ER * 0.8 + PGR * 1.2 * Bcl2 + SCUBE2) / 4
-ProliferationGroupScore = (Survivin + Ki67 + MYBL2 + CyclinB1 + STK15) / 5
-if (ProliferationGroupScore < 6.5) {
-    ProliferationGroupScore = 6.5
-}
-InvasionGroupScore = (CathepsinL2 + Stromelysin3) / 2
-
-RSu = 0.47 * HER2GroupScore
-    - 0.34 * ERGroupScore
-    + 1.04 * ProliferationGroupScore
-    + 0.10 * InvasionGroupScore
-    + 0.05 * CD68
-    - 0.08 * GSTM1
-    - 0.07 * BAG1
--- www/scripts/js/upload.js
+++ www/scripts/js/upload.js
@@ -3,11 +3,11 @@ let chosenImage = document.getElementById("chosen-image");
 let fileName = document.getElementById("file-name");
 let uploadArea = document.querySelector(".upload-area");
 let error = document.getElementById("error");
-
+let data = {};
 const fileHandler = (file, name, type) => {
   if (type !== "text/csv") {
     //File Type Error
-    error.innerText = "Please upload an csv file";
+    error.innerText = "Please upload a csv file";
     return false;
   }
   error.innerText = "";
@@ -15,15 +15,21 @@ const fileHandler = (file, name, type) => {
   reader.readAsText(file);
   reader.onloadend = (event) => {
     const lines = event.target.result.split('\n');
-
     for (let line of lines) {
         const [field1, field2] = line.split(',');
         if (field1 && field2) { // 空行を無視
             let eleId="val-" + field1;
             let elem = document.getElementById(eleId);
+            data[field1] = parseFloat(field2);
             elem.textContent = field2;
         }
     }
+    let data_ = calc_rsu(data)
+    document.getElementById("valg-PROLIFERATION").textContent = data_["g-PROLIFERATION"];
+    document.getElementById("valg-ESTROGEN").textContent = data_["g-ESTROGEN"];
+    document.getElementById("valg-HER2").textContent = data_["g-HER2"];
+    document.getElementById("valg-INVASION").textContent = data_["g-INVASION"];
+    document.getElementById("val-RSU").textContent = data_["RSU"];
   };
 };
 
